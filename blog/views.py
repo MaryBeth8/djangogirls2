e6878436ff9ebf9f9f27d1432d8900eb0c990e65
marybeth <marybeth.mclendon@gmail.com>
2018-05-28 17:33:28 -0700

    add link in base.html, add new url in urls.py, add view in views.py, create new post_draft_list.html doc for handling post drafts

--- blog/views.py
+++ blog/views.py
@@ -18,7 +18,8 @@ def post_new(request):
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
+			#post.published_date = timezone.now()
+			#removed this line so that a new post is saved as a draft rather than published immediately
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
@@ -32,9 +33,15 @@ def post_edit(request, pk):
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
+			#post.published_date = timezone.now()
+			#removed this line so that an edited post is saved as a draft rather than published immediately
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm(instance=post)
 	return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
