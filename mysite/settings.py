cbd57d2ce98c95bac6a7b385479ec50ea6f00a26
marybeth <marybeth.mclendon@gmail.com>
2018-05-30 13:55:37 -0700

    complete PostgreSQL installation, update DATABASES section of mysite/settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,8 +79,12 @@ LOGIN_REDIRECT_URL = '/'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls2',
+        'USER': 'mbeth',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
